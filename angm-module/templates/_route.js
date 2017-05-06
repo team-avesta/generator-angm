2edaeeab8397418cd74657d75190673b63daf5c9
Chirag <chiraghindocha1507@gmail.com>
2017-05-06 19:14:37 +0530

    made foll changes: made route file function base

--- angm-module/templates/_route.js
+++ angm-module/templates/_route.js
@@ -1,31 +1,37 @@
-'use strict';
+(function() {
+    'use strict';
 
-/**
- * @ngdoc function
- * @name app.route:<%= slugifiedName %>Route
- * @description
- * # <%= slugifiedName %>Route
- * Route of the app
- */
+    /**
+     * @ngdoc function
+     * @name app.route:<%= slugifiedName %>Route
+     * @description
+     * # <%= slugifiedName %>Route
+     * Route of the app
+     */
 
-angular.module('<%= slugifiedName %>')
-	.config(['$stateProvider', function ($stateProvider) {
-		<% if (angularMaterial) { %>
-		$stateProvider
-			.state('dashboard.<%= slugifiedName %>', {
-				url:'/<%= slugifiedName %>',
-				templateUrl: 'app/modules/<%= slugifiedName %>/<%= slugifiedName %>.html',
-				controller: '<%= slugifiedNameCapitalize %>Ctrl',
-				controllerAs: 'vm'
-			});
+    angular.module('<%= slugifiedName %>')
+        .config(<%= slugifiedNameCapitalize %>);
 
-		<% } else { %>
-		$stateProvider
-			.state('<%= slugifiedName %>', {
-				url:'/<%= slugifiedName %>',
-				templateUrl: 'app/modules/<%= slugifiedName %>/<%= slugifiedName %>.html',
-				controller: '<%= slugifiedNameCapitalize %>Ctrl',
-				controllerAs: 'vm'
-			});
-		<% } %>
-	}]);
+    <%= slugifiedNameCapitalize %>.$inject = ['$stateProvider'];
+
+    function <%= slugifiedNameCapitalize %> ($stateProvider) {
+        <% if (angularMaterial) { %>
+        $stateProvider
+            .state('dashboard.<%= slugifiedName %>', {
+                url: '/<%= slugifiedName %>',
+                templateUrl: 'app/modules/<%= slugifiedName %>/<%= slugifiedName %>.html',
+                controller: '<%= slugifiedNameCapitalize %>Ctrl',
+                controllerAs: 'vm'
+            });
+
+        <% } else { %>
+        $stateProvider
+            .state('<%= slugifiedName %>', {
+                url: '/<%= slugifiedName %>',
+                templateUrl: 'app/modules/<%= slugifiedName %>/<%= slugifiedName %>.html',
+                controller: '<%= slugifiedNameCapitalize %>Ctrl',
+                controllerAs: 'vm'
+            });
+        <% } %>
+    }
+})();
--- app/templates/app/modules/dashboard/dashboardRoute.js
+++ app/templates/app/modules/dashboard/dashboardRoute.js
@@ -1,27 +1,33 @@
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('dashboard')
-  .config(['$stateProvider', function($stateProvider) {
-    $stateProvider
-      <% if (angularMaterial) { %>
-      .state('dashboard', {
-        url: '',
-        abstract: true,
-        templateUrl: 'app/modules/dashboard/dashboard.html',
-        controller: 'DashboardCtrl',
-        controllerAs: 'vm'
-      })
-      .state('dashboard.home', {
-        url: '/home',
-        //template: '<ui-view/>'
-        templateUrl: 'app/modules/dashboard/home.html'
-      });
-    <% } else { %>
-    .state('home', {
-      url: '/',
-      templateUrl: 'app/modules/home/home.html',
-      controller: 'HomeCtrl',
-      controllerAs: 'vm'
-    });
-    <% } %>
-  }]);
+    angular.module('dashboard')
+        .config(Dashboard);
+
+    Dashboard.$inject = ['$stateProvider'];
+
+    function Dashboard($stateProvider) {
+        $stateProvider
+            <% if (angularMaterial) { %>
+            .state('dashboard', {
+                url: '',
+                abstract: true,
+                templateUrl: 'app/modules/dashboard/dashboard.html',
+                controller: 'DashboardCtrl',
+                controllerAs: 'vm'
+            })
+            .state('dashboard.home', {
+                url: '/home',
+                //template: '<ui-view/>'
+                templateUrl: 'app/modules/dashboard/home.html'
+            });
+        <% } else { %>
+        .state('home', {
+            url: '/',
+            templateUrl: 'app/modules/home/home.html',
+            controller: 'HomeCtrl',
+            controllerAs: 'vm'
+        });
+        <% } %>
+    }
+})();
